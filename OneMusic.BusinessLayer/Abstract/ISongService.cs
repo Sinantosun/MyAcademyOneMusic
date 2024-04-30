0a30a43a579422db83cfd3fe575183c188263d8d
Sinantosun <sinan_tosun4141@hotmail.com>
2024-04-30 22:06:24 +0300

    Add project files.

---
+++ OneMusic.BusinessLayer/Abstract/ISongService.cs
@@ -0,0 +1,14 @@
+﻿using OneMusic.DataAccessLayer.Repositories;
+using OneMusic.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMusic.BusinessLayer.Abstract
+{
+    public interface ISongService : IGenericService<Song>
+    {
+    }
+}
---
+++ OneMusic.WebUI/Program.cs
@@ -0,0 +1,45 @@
+using OneMusic.BusinessLayer.Abstract;
+using OneMusic.BusinessLayer.Concrete;
+using OneMusic.DataAccessLayer.Abstract;
+using OneMusic.DataAccessLayer.Concrete;
+using OneMusic.DataAccessLayer.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+builder.Services.AddScoped<IAboutService, AboutManager>();
+
+builder.Services.AddScoped<IAlbumDal, EFAlbumDal>();
+builder.Services.AddScoped<IAlbumService, AlbumManager>();
+
+builder.Services.AddScoped<IBannerDal, EfBannerDal>();
+builder.Services.AddScoped<IBannerService, BannerManager>();
+
+builder.Services.AddDbContext<OneMusicContext>();
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
